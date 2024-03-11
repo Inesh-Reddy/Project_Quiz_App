bbe083c4a598216f0641188dbf4aadae022fa3f5
Inesh-Reddy <ineshreddy007@gmail.com>
2024-03-11 19:09:18 +0530

    adding backend code to add and fetch questions to database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require('body-parser');
+const PORT = 3000;
 const app = express();
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+const { withAccelerate } = require('@prisma/extension-accelerate');
+const { channel } = require('diagnostics_channel');
+const prismaAccelerateUrl = process.env.PRISMA_ACCELERATE_URL;
 
-app.use(express());
+
+const prisma = new PrismaClient({
+    datasources: { db: { url: prismaAccelerateUrl } },
+}).$extends(withAccelerate());
+
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
+
+app.post("/create-question", async function(req, res) {
+    const { text, choices, answerIndex } = req.body;
+
+    try {
+        const question = await prisma.question.create({
+            data: {
+                text,
+                choices,
+                answerIndex
+            }
+        });
+
+        return res.json({
+            qid: question.qid
+        });
+    } catch (error) {
+        console.error("Error creating question:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+});
+
+app.get("/question", async function(req, res) {
+    try{
+        const question = await prisma.question.findFirst({
+            where:{
+                qid:1
+            }
+        });
+        return res.json({
+            question: question.text,
+            choise: question.choices
+
+        })
+    }catch(error){
+        console.log("error fetching question:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+    res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+    console.log('Server listening on port', PORT);
+});
---
+++ backend/prisma/migrations/20240311124421_init_schema/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Answer" (
+    "aid" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "questionId" INTEGER NOT NULL,
+    "userAnswer" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("aid")
+);
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("qid") ON DELETE RESTRICT ON UPDATE CASCADE;
